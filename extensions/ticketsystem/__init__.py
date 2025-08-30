3d8aae73433ab8e9ce3a63e6d669e9d4d237dd7c
murpi <60477660+murpii@users.noreply.github.com>
2025-08-30 21:46:54 +0200

    Send Report ticket views after restart

--- extensions/ticketsystem/__init__.py
+++ extensions/ticketsystem/__init__.py
@@ -5,6 +5,7 @@ from .views import categories, inner_buttons, confirm, subscribe
 async def setup(bot):
     bot.add_view(view=categories.MainMenu(bot))
     bot.add_view(view=inner_buttons.BaseTicketButtons(bot))
+    bot.add_view(view=inner_buttons.ReportTicketButtons(bot))
     bot.add_view(view=inner_buttons.BanAppealTicketButtons(bot))
     bot.add_view(view=inner_buttons.RenameTicketButtons(bot))
     bot.add_view(view=confirm.ConfirmView(bot))
